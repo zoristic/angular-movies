c497a7624121269139a48837f1ee0b8214b3c092
zoristic <sokyyy@gmail.com>
2022-03-13 09:05:18 +0100

    Zavrseno

--- src/app/actors/actors-autocomplite/actors-autocomplite.component.ts
+++ src/app/actors/actors-autocomplite/actors-autocomplite.component.ts
@@ -1,34 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-actors-autocomplite',
   templateUrl: './actors-autocomplite.component.html',
-  styleUrls: ['./actors-autocomplite.component.css']
+  styleUrls: ['./actors-autocomplite.component.css'],
 })
 export class ActorsAutocompliteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  control: FormControl =new FormControl();
+  control: FormControl = new FormControl();
 
   actors = [
-    {name: 'Tom Holland', picture: 'https://m.media-amazon.com/images/M/MV5BNzZiNTEyNTItYjNhMS00YjI2LWIwMWQtZmYwYTRlNjMyZTJjXkEyXkFqcGdeQXVyMTExNzQzMDE0._V1_UX214_CR0,0,214,317_AL_.jpg'},
-    {name: 'Tom Hanks', picture: 'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg'},
-    {name: 'Samuel L Jackson', picture: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'}
+    {
+      name: 'Tom Holland',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BNzZiNTEyNTItYjNhMS00YjI2LWIwMWQtZmYwYTRlNjMyZTJjXkEyXkFqcGdeQXVyMTExNzQzMDE0._V1_UX214_CR0,0,214,317_AL_.jpg',
+    },
+    {
+      name: 'Tom Hanks',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg',
+    },
+    {
+      name: 'Samuel L Jackson',
+      picture:
+        'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg',
+    },
   ];
 
-  selectedActors = [];
+  selectedActors: string[] = [];
+  originalActros = this.actors;
+  columnsToDisplay = ['picture', 'name', 'character', 'actions'];
+
+  @ViewChild(MatTable) table: MatTable<any> | any;
 
   ngOnInit(): void {
+    this.control.valueChanges.subscribe((value) => {
+      this.actors = this.originalActros;
+      this.actors = this.actors.filter(
+        (actor) => actor.name.indexOf(value) !== -1
+      );
+    });
   }
 
-  optionnSelected(event: MatAutocompleteSelectedEvent){
-
+  optionSelected(event: MatAutocompleteSelectedEvent) {
     this.selectedActors.push(event.option.value);
     this.control.patchValue('');
-    
+    if (this.table !== undefined) {
+      this.table.renderRows();
+    }
   }
 
+  remove(actor: any) {
+    const index = this.selectedActors.findIndex((a) => a === actor.name);
+    this.selectedActors.splice(index, 1);
+    this.table.renderRow();
+  }
+
+  dropped(event: CdkDragDrop<any[]>) {
+    const previousIndex = this.selectedActors.findIndex(
+      (actor) => actor === event.item.data
+    );
+    moveItemInArray(this.selectedActors, previousIndex, event.currentIndex);
+    this.table.renderRows();
+  }
 }
